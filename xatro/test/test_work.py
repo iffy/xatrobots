3151a8fdc807a922b7dbadd5e52b354114c91677
Matt Haggard <haggardii@gmail.com>
2013-08-28 11:28:53 -0600

    Add WorkMaker

---
+++ xatro/test/test_work.py
@@ -0,0 +1,96 @@
+from twisted.trial.unittest import TestCase
+from itertools import product
+from hashlib import sha1
+
+from xatro.work import WorkMaker, Work
+
+
+POOL = map(chr, xrange(0, 255))
+MAX_SHA = int('f'*40, 16)
+
+
+def validAnswer(nonce, difficulty, scale, answer):
+    result = int(sha1(nonce + answer).hexdigest(), 16)
+    threshold = (scale - difficulty) * (MAX_SHA / scale)
+    return result > threshold
+
+
+def _options(pool):
+    """
+    Return gradually lengthening selections of pool.
+
+    For instance, if pool is ABCD, return
+
+        A, B, C, D, AA, AB, AC, AD, BA, BB, BC, BD
+    """
+    i = 1
+    while True:
+        for p in product(pool, repeat=i):
+            yield ''.join(p)
+        i += 1
+
+
+def findResult(nonce, difficulty, scale):
+    """
+    Brute force a string that will satisfy the problem for the given args.
+    """
+    for o in _options(POOL):
+        if validAnswer(nonce, difficulty, scale, o):
+            return o
+    return ''
+
+
+
+class WorkMakerTest(TestCase):
+
+
+    def test_getWork(self):
+        """
+        You can get a piece of work.
+        """
+        maker = WorkMaker()
+        work = maker.getWork()
+        self.assertEqual(work.difficulty, maker.difficulty)
+        self.assertEqual(work.scale, maker.scale)
+        self.assertNotEqual(work.nonce, None)
+        self.assertNotEqual(work.nonce, '')
+
+
+    def test_getWork_custom(self):
+        """
+        You can customize the difficulty and scale of the work requested.
+        """
+        maker = WorkMaker()
+        work = maker.getWork(9, 99)
+        self.assertEqual(work.difficulty, 9)
+        self.assertEqual(work.scale, 99)
+
+
+    def test_getWork_differentNonce(self):
+        """
+        The nonce should be different each time
+        """
+        maker = WorkMaker()
+        a = maker.getWork()
+        b = maker.getWork()
+        self.assertNotEqual(a.nonce, b.nonce)
+
+
+    def test_isResult(self):
+        """
+        You can verify that the result of some work is valid
+        """
+        maker = WorkMaker()
+        work = maker.getWork()
+        pool = map(chr, xrange(0, 255))
+        result = findResult(work.nonce, work.difficulty, work.scale)
+        self.assertEqual(maker.isResult(work, result), True)
+
+
+    def test_isResult_False(self):
+        """
+        isResult should detect bad results and return False
+        """
+        maker = WorkMaker()
+        work = Work(1, 100000, 'foo')
+        self.assertEqual(maker.isResult(work, 'hey'), False)        
---
+++ xatro/work.py
@@ -0,0 +1,60 @@
+from collections import namedtuple
+from uuid import uuid4
+from hashlib import sha1
+
+
+Work = namedtuple('Work', ['difficulty', 'scale', 'nonce'])
+
+
+
+class WorkMaker(object):
+    """
+    I make work and verify its completeness.
+
+    @ivar difficulty: Default difficulty if none is provided to L{getWork}.
+    @ivar scale: Default scale if none is provided to L{getWork}.
+    """
+
+    difficulty = 10
+    scale = 10000
+
+    MAX_SHA = int('f'*40, 16)
+
+
+    def getWork(self, difficulty=None, scale=None):
+        """
+        Get some L{Work} to do.
+
+        @param difficulty: Difficulty of the work.  If not provided, this will
+            be the C{difficulty} set on the instance.
+        @type difficulty: int
+
+        @param scale: Scale of the work.  If not provided, this will be the
+            C{scale} set on the instance.
+        @type scale: int
+
+        @return: A L{Work} instance.
+        """
+        nonce = sha1(str(uuid4())).hexdigest()
+        return Work(difficulty or self.difficulty, scale or self.scale, nonce)
+
+
+    def isResult(self, work, result):
+        """
+        Determine if the given C{result} is an acceptable solution for the
+        C{work}.
+
+        @return: C{True} if it's acceptable, else C{False}
+        """
+        return self._validAnswer(work.nonce, work.difficulty, work.scale,
+                                 result)
+
+
+    def _validAnswer(self, nonce, difficulty, scale, answer):
+        """
+        Verify that the given C{answer} produces a hash greater than the
+        threshold defined by C{difficulty} and C{scale}.
+        """
+        result = int(sha1(nonce + answer).hexdigest(), 16)
+        threshold = (scale - difficulty) * (self.MAX_SHA / scale)
+        return result > threshold
