b258029950d3270e024cdac3136ae8aec814eeb4
Matt Haggard <haggardii@gmail.com>
2013-09-05 10:18:07 -0600

    Add State object (keeps track of state from events)

---
+++ xatro/state.py
@@ -0,0 +1,76 @@
+
+from xatro.event import Created, Destroyed, AttrSet, ItemAdded, ItemRemoved
+from functools import wraps
+
+
+class Mapper(object):
+
+
+    def __init__(self, mapping=None, instance=None):
+        self._mapping = mapping or {}
+        self._instance = instance
+
+
+    def __get__(self, instance, type=None):
+        return Mapper(self._mapping, instance)
+
+
+    def call(self, key, *args, **kwargs):
+        func = self._mapping[key]
+        return func(self._instance, *args, **kwargs)
+
+
+    def handle(self, key):
+        def deco(f):
+            self._mapping[key] = f
+            return f
+        return deco
+
+
+
+class State(object):
+    """
+    I hold the state of a world as built up by events.
+
+    @ivar state: Dictionary of all the objects in the world.
+    """
+
+    mapper = Mapper()
+
+
+    def __init__(self):
+        self.state = {}
+
+
+    def eventReceived(self, event):
+        return self.mapper.call(event.__class__, event)
+
+
+    @mapper.handle(Created)
+    def handle_Created(self, event):
+        self.state[event.id] = {
+            'id': event.id,
+        }
+
+
+    @mapper.handle(Destroyed)
+    def handle_Destroyed(self, event):
+        self.state.pop(event.id)
+
+
+    @mapper.handle(AttrSet)
+    def handle_AttrSet(self, (id, name, value)):
+        self.state[id][name] = value
+
+
+    @mapper.handle(ItemAdded)
+    def handle_ItemAdded(self, (id, name, value)):
+        obj = self.state[id]
+        if name not in obj:
+            obj[name] = []
+        obj[name].append(value)
+
+
+    @mapper.handle(ItemRemoved)
+    def handle_ItemRemoved(self, (id, name, value)):
+        self.state[id][name].remove(value)
---
+++ xatro/test/test_state.py
@@ -0,0 +1,64 @@
+from twisted.trial.unittest import TestCase
+
+
+from xatro.state import State
+from xatro.event import Created, Destroyed, AttrSet, ItemAdded, ItemRemoved
+
+
+
+class StateTest(TestCase):
+
+
+    def test_Created(self):
+        """
+        When an object is created, add it to the state dict.
+        """
+        state = State()
+        state.eventReceived(Created('foo'))
+        self.assertEqual(state.state['foo'], {'id': 'foo'})
+
+
+    def test_Destroyed(self):
+        """
+        When an object is destroyed, remove it from the state dict.
+        """
+        state = State()
+        state.eventReceived(Created('foo'))
+        state.eventReceived(Destroyed('foo'))
+        self.assertEqual(state.state, {})
+
+
+    def test_AttrSet(self):
+        """
+        When an attribute is set, it should be set in the state dict.
+        """
+        state = State()
+        state.eventReceived(Created('foo'))
+        state.eventReceived(AttrSet('foo', 'theattr', 4))
+        self.assertEqual(state.state['foo']['theattr'], 4)
+
+
+    def test_ItemAdded(self):
+        """
+        When an item is added, it should be added to a list attribute on the
+        object.
+        """
+        state = State()
+        state.eventReceived(Created('foo'))
+        state.eventReceived(ItemAdded('foo', 'thelist', 10))
+        self.assertEqual(state.state['foo']['thelist'], [10])
+
+        state.eventReceived(ItemAdded('foo', 'thelist', 20))
+        self.assertEqual(state.state['foo']['thelist'], [10, 20])
+
+
+    def test_ItemRemoved(self):
+        """
+        When an item is removed, it should be removed from the list attribute
+        on the object.
+        """
+        state = State()
+        state.eventReceived(Created('foo'))
+        state.eventReceived(ItemAdded('foo', 'thelist', 10))
+        state.eventReceived(ItemRemoved('foo', 'thelist', 10))
+        self.assertEqual(state.state['foo']['thelist'], [])
